feac130e31383d8a1529f4d476f69c4a2ebd7b55
John Corry <johnecorry@gmail.com>
2022-05-01 11:59:49 +0100

    footer initial work

--- src/main/components/layout/footer/index.tsx
+++ src/main/components/layout/footer/index.tsx
@@ -1,7 +1,8 @@
+import "./styles.css";
 import React from "react";
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return <p className="footer">Designed & Developed by J. Corry 2022</p>;
 };
 
 export default Footer;
