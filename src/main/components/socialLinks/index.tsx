8b2f056ef482e7dc9a766bb49e20e9eeec73111f
John Corry <johnecorry@gmail.com>
2022-05-01 14:00:27 +0100

    social links added to footer

---
+++ src/main/components/socialLinks/index.tsx
@@ -0,0 +1,24 @@
+import {
+  Email,
+  GitHub,
+  Instagram,
+  LinkedIn,
+  Twitter,
+  YouTube,
+} from "@mui/icons-material";
+import React from "react";
+
+const SocialLinks = () => {
+  return (
+    <div>
+      <LinkedIn />
+      <Instagram />
+      <GitHub />
+      <Twitter />
+      <YouTube />
+      <Email />
+    </div>
+  );
+};
+
+export default SocialLinks;
