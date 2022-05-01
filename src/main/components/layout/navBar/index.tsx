2eae510f149920ecd22ced41b5067846b8c37e40
John Corry <johnecorry@gmail.com>
2022-05-01 11:48:42 +0100

    navBar initial work

--- src/main/components/layout/navBar/index.tsx
+++ src/main/components/layout/navBar/index.tsx
@@ -1,13 +1,19 @@
+import "./styles.css";
+
 import React from "react";
 import NavButton from "../../navButton";
 
 const NavBar: React.FC = () => {
   return (
-    <nav>
-      <NavButton linkTo="/" buttonText="John." />
-      <NavButton linkTo="work" buttonText="Work" />
-      <NavButton linkTo="me" buttonText="Me." />
-      <NavButton linkTo="get-in-touch" buttonText="Get In Touch." />
+    <nav className="navBar-wrapper">
+      <div className="navBar-leftSide">
+        <NavButton linkTo="/" buttonText="John." />
+      </div>
+      <div className="navBar-rightSide">
+        <NavButton linkTo="work" buttonText="Work" />
+        <NavButton linkTo="me" buttonText="Me." />
+        <NavButton linkTo="get-in-touch" buttonText="Get In Touch." />
+      </div>
     </nav>
   );
 };
--- src/main/components/navButton/index.tsx
+++ src/main/components/navButton/index.tsx
@@ -1,3 +1,5 @@
+import "./styles.css";
+
 import { Button } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
@@ -9,9 +11,11 @@ interface NavButtonProps {
 
 const NavButton: React.FC<NavButtonProps> = ({ linkTo, buttonText }) => {
   return (
-    <Button component={Link} to={linkTo} variant="contained">
-      {buttonText}
-    </Button>
+    <div className="navButton">
+      <Button component={Link} to={linkTo} sx={{ color: "black" }}>
+        {buttonText}
+      </Button>
+    </div>
   );
 };
 
