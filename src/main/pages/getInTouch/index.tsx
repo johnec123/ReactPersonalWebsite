c666b0742acb7d73251f991b26835deb46fca245
John Corry <johnecorry@gmail.com>
2022-05-08 10:19:09 +0100

    validation added

--- src/main/pages/getInTouch/index.tsx
+++ src/main/pages/getInTouch/index.tsx
@@ -2,11 +2,14 @@ import "./styles.css";
 
 import React from "react";
 import ContactForm from "../../components/contactForm";
+import ContactText from "../../components/contactText";
 
 const GetInTouch: React.FC = () => {
   return (
     <div className="getInTouch-wrapper">
-      <div className="getInTouch-aside">Get In Touch</div>
+      <div className="getInTouch-aside">
+        <ContactText />
+      </div>
       <div className="getInTouch-aside">
         <ContactForm />
       </div>
